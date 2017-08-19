40257ee75dccb3410d244028925b2bfa9c559aae
nux <nux@example.com>
2017-08-19 16:23:12 -0700

    tons of changes, breathe lighting for drying mode, turbo mode, different lighting maps for reverse and lots of other code organization changes.

--- powerWrapper/powerWrapper.ino
+++ powerWrapper/powerWrapper.ino
@@ -4,10 +4,13 @@
 //August 2017
 #include <OneButton.h>
 #include <Adafruit_NeoPixel.h>
+#include <math.h>   // for breathe routine
 #define PIN 6
 
-const int ledPin =  7;  // Led on power switch.
-int ledState = LOW;
+const int ledPin =  9;  // Led on key and power switch.
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;
+
 unsigned long previousMillis = 0;
 long interval = 1000;
 
@@ -23,6 +26,8 @@ const int reversePin = 12;       // White Wire in ClearPath Cable
 const int clearPathPin = 10;    // Black Wire in ClearPath Cable 
 const int enablePin = 11;       // Blue Wire in ClearPath Cable
 int val;    // variable to read the value from the analog pin
+int throttle=0;  //mapped value derived from val
+int turboCut=80;  //End of the Pedal span;  point at which turbo kicks in
 int drySpeed=4;
 int light = 0;
 
@@ -37,16 +42,20 @@ int light = 0;
 //0,116,255        light blue
 //5,0,255            dark blue 
 
-int r[] = {  0,  0,255,255,  0,  5,  0,255,255,255,255,255,255 };
-int g[] = {255,255,255,255,116,  0,255,255,120,  0,  0, 14,0};
-int b[] = { 16, 16,  0,  0,255,255, 16,  0,  0,  0,112,240,0};
+int r[] = {  0,  0, 51,153,255,255,204,204,255,255,153, 51,204,153,102, 51,  0,  0,  0,  0, 51,102,153,204,255};
+int g[] = {  0,  0, 51,153,204,153,102,204,255,255,255,255,255,255,255,255,255,204,204,255,255,255,255,255,204};
+int b[] = {204,204,255,255,153, 51,  0,  0, 51,153,255,255,204,153,102, 51,  0,  0,204,255,255,255,255,255,259};
+
+int rr[] = {153,153,204,255,255,255,255,255,255,255,255,204,153,102, 51,  0,  0,  0,  0, 51,102,153,204,255,255};
+int gg[] = {  0,  0,  0,  0, 51,153,204,255,255,255,255,255,255,255,255,255,204,204,255,255,255,255,255,204,153};
+int bb[] = {  0,  0,  0,  0, 51,153,204,204,153,102, 51,204,153,102, 51,  0,  0,204,255,255,255,255,255,259,204};
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   pinMode(reversePin, OUTPUT);
   pinMode(enablePin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
   Serial.print("Serial comms initialized \n\n");
   strip.begin();
@@ -62,93 +71,92 @@ void speedometer(uint32_t c, uint8_t wait) {
         for (int i=0;i<24; i++) {                            // 9 is the number of pixels in the ring
           if (light>=i)  {
             delay(10);
-            strip.setPixelColor(i, c);   
-            Serial.print("lighting: ");
-            Serial.print(i);
-            Serial.print("\n");                        
+            strip.setPixelColor(i, c);                      
           }
           else {
             strip.setPixelColor(i, 0);
-
           }
         }
       strip.show();
 }
 
-void blink() {
-     unsigned long currentMillis = millis();
-    if (dryMode == true){
-        interval = 330;
-    }
-    else{
-        interval = 1000;
-    }
-    if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+void breathe() {            //  LIGHT BREATHING FUNCTION                    
+  // set the brightness of pin 9:
+  analogWrite(ledPin, brightness);
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
 
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 105) {
+    fadeAmount = -fadeAmount;
   }
+  // wait for 30 milliseconds to see the dimming effect
+  strip.setBrightness(brightness);
 }
 
-void reverse() {
-    if (rotation == false) {
-    digitalWrite(reversePin, HIGH);
-  } else {
-    digitalWrite(reversePin, LOW);
-  }
-} //reverse
 
+void setRotation(){                                 // HANDLES THE DIFFERENT ROTATION MODES
+  
+    if (rotation == false) {         //  FORWARED ROTATION DIRECTION
+       digitalWrite(reversePin, HIGH);
+       speedometer(strip.Color(r[light], g[light], b[light]), 5); //  paint speedometer with FORWARD rotation color set
+       analogWrite(ledPin, 255);   // Led Lights OFF
+    } 
+    else {
+       digitalWrite(reversePin, LOW);   //  REVERSE ROTATION DIRECTION
+       analogWrite(ledPin, 0);   // Led Lights ON
+       speedometer(strip.Color(rr[light], gg[light], bb[light]), 5); //  paint speedometer with REVERSE rotation color set 
+    }
+}
 void singleclick() {
-  if (drySpeed>23){
+  rotation = !rotation;                                        //   SWITCH THE ROTATION DIRECTION 
+} //                    singleclick: reverses rotation
+
+void doubleclick() {
+    if (drySpeed>23){
     drySpeed=1;
-  }
+  }                                                                                                                 
   else{
     drySpeed++;
   }
-} // click1
-
-void doubleclick() {
-  rotation = !rotation;
-} // doubleclick
+} //                    doubleclick: increases dry speed
 
 void longpress() {
 dryMode = !dryMode;
-} // longpress
+} //                    longpress: toggles drying mode
 
 void loop() { 
   button.tick();  // keep watching the push button:
   digitalWrite(enablePin,HIGH);
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 235);     // scale it to use it with the servo (value between 0 and 180)
-  if (dryMode == false){
-         analogWrite(clearPathPin, val);
-         light = map(val, 0, 235, 0, 23);    // determines the number of lights to energize
-  }
-  else{
-         if (val <= 50){
-          analogWrite(clearPathPin, drySpeed);   // remember if you change drySpeeds max, then this must be changed
-          light=map(drySpeed,1,24,0,23) ;   
+  throttle = map(val, 0, 1023, 0, turboCut);     // scale it to use it with the servo (value between 0 and 180)
+  setRotation();
+
+  //  DRY MODE LOGIC
+  if (dryMode == false){             // NORMAL PEDAL DRIVEN WRAPPING MODE IS ACTIVE
+         analogWrite(clearPathPin, throttle);
+         if (throttle < turboCut - 10);{
+            light = map(throttle, 0, turboCut, 0, 23);    // determines the number of lights to energize
          }
-         else{
+         if (throttle > turboCut - 10){
+            analogWrite(clearPathPin, 250);
+         }
+  }
+  else{                             //  DRYING MODE IS ACTIVE
+         if (throttle <= 50){                       // PEDAL IS NOT TOUCHED;  DRYING MODE IS GOOD TO GO
+          analogWrite(clearPathPin, drySpeed);   // remember if you change drySpeeds max, then this must be changed
+          light=map(drySpeed,1,24,0,23) ; 
+          breathe();
+         }      
+         else{                                 // PEDAL HAS BEEN STOMPED ON;   STOP DRYING
           analogWrite(clearPathPin, 0);
           light=0;
+          analogWrite(ledPin, 0);
          }
   }
-  speedometer(strip.Color(r[light], g[light], b[light]), 5); // Red
-  delay(10);                           // waits for the servo to get there
-  blink();
-  reverse();  
-
-
+  
+  delay(10);                           // PROGRAM DELAY
 
 
 }
